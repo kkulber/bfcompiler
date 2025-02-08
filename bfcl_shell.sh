d7a32ca80326e7035e53d274ce9cd5715f040142
Kristof Kulber <kristofkulber@gmail.com>
2025-02-08 14:41:32 +0100

    Added bfcl shell

---
+++ bfcl_shell.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Files 
+BFCL_FILE="programs/tmp_shell.bfcl"
+BF_FILE="generated/tmp_shell.bf"
+
+# Create shell program
+> "$BFCL_FILE"
+
+echo "Bfcolang shell started. Enter command or type \"exit\" to quit."
+
+previous_cmd=""
+
+while true; do
+    # Command input
+    read -p "# " CMD
+    
+    # Exit
+    if [[ "$CMD" == "exit" ]]; then
+        echo "Exiting shell."
+        rm -f "$BFCL_FILE"
+        rm -f "$BF_FILE"
+        break
+    fi
+    
+    # Remove last print if it exists
+    if [[ -s "$BFCL_FILE" ]]; then
+        sed -i '$d' "$BFCL_FILE"
+    fi
+    
+    # Add previous command without print
+    if [[ -n "$previous_cmd" ]]; then
+        echo "$previous_cmd;" >> "$BFCL_FILE"
+    fi
+    
+    # Print the current command
+    echo ".($CMD);" >> "$BFCL_FILE"
+    
+    # Store previous command
+    previous_cmd="$CMD"
+    
+    # Run
+    if ! python3 bfcolang.py "$BFCL_FILE" > /dev/null; then
+        sed -i '$d' "$BFCL_FILE"   # Remove the current command line
+        sed -i '$d' "$BFCL_FILE"   # Remove the previous command line
+        continue
+    fi
+    ./bfinterpreter "$BF_FILE"
+
+done
